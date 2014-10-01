110140937649f566335f9e2f69aab05f0ad1b9a1
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-10-01 16:57:02 -0300

    Criação do BD.java

---
+++ src/trabalho02/controlador/BD.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trabalho02.controlador;
+
+import baseTolkien.Entidades.Biblioteca;
+
+/**
+ *
+ * @author Danilo Medeiros Eler
+ */
+public class BD {
+    public static Biblioteca biblioteca = new Biblioteca();
+}
