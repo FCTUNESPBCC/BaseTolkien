4df557b616a2fb3bd94183019111edda26e9625c
Leonardo Dias <leonardo.dias@multimidiaeducacional.com.br>
2014-10-02 17:11:16 -0300

    Alteração Biblioteca.java
    Remoção de todos os métodos de busca avançada

--- src/baseTolkien/Entidades/Biblioteca.java
+++ src/baseTolkien/Entidades/Biblioteca.java
@@ -50,137 +50,16 @@ public class Biblioteca {
         this.livros.add(livro);
     }
     
+    public void addEmprestimo(Emprestimo emprestimo){
+        this.emprestimos.add(emprestimo);
+    }
+    
     public ArrayList<Usuario> getAllUsuario(){
         return usuarios;
     }
     
-    public ArrayList<Usuario> getAllAluno(){
-        ArrayList<Usuario> usuarioPesquisa = new ArrayList<Usuario>();
-        for (Usuario usuario : usuarios) {
-            if (usuario instanceof Aluno) {
-                usuarioPesquisa.add(usuario);
-            }
-        }
-        return usuarioPesquisa;
-    }
-    
-    public ArrayList<Usuario> getAllProfessor(){
-        ArrayList<Usuario> usuarioPesquisa = new ArrayList<Usuario>();
-        for (Usuario usuario : usuarios) {
-            if (usuario instanceof Professor) {
-                usuarioPesquisa.add(usuario);
-            }
-        }
-        return usuarioPesquisa;
-    }
-    
     public ArrayList<Livro> getAllLivros(){
         return livros;
-    }
-    
-    public ArrayList<Item> getAllLivrosOf(String codUsuario){
-        ArrayList<Item> livroPesquisa = new ArrayList<Item>();
-        for(Emprestimo emprestimo: emprestimos){
-            if(emprestimo.getCodUsuario().equals(codUsuario)){
-                for(Item item: emprestimo.getItens())
-                    livroPesquisa.add(item);
-            }
-        }
-        return livroPesquisa;
-    }
-    
-    public ArrayList<Item> getAllLivrosNaoDevolvidosOf(String codUsuario){
-        ArrayList<Item> livroPesquisa = new ArrayList<Item>();
-        for(Emprestimo emprestimo: emprestimos){
-            if(emprestimo.getCodUsuario().equals(codUsuario)){
-                for(Item item: emprestimo.getItens())
-                    if(!item.isDevolvido())
-                        livroPesquisa.add(item);
-            }
-        }
-        return livroPesquisa;
-    }
-    
-    public ArrayList<Livro> getAllLivroDisponivel(){
-        ArrayList<Livro> livroPesquisa = new ArrayList<Livro>();
-        for (Livro livro : livros) {
-            if (!livro.isEmprestado()) {
-                livroPesquisa.add(livro);
-            }
-        }
-        return livroPesquisa;
-    }
-    
-    public ArrayList<Item> getAllLivroEmprestado(){
-        ArrayList<Item> livroPesquisa = new ArrayList<Item>();
-        for (Emprestimo emprestimo : emprestimos) {
-            for(Item item: emprestimo.getItens())
-            if (!item.isDevolvido()) {
-                livroPesquisa.add(item);
-            }
-        }
-        return livroPesquisa;
-    }
-    
-    public ArrayList<Item> getAllLivroAtrasado(){
-        ArrayList<Item> livroPesquisa = new ArrayList<Item>();
-        for (Emprestimo emprestimo : emprestimos) {
-            for(Item item: emprestimo.getItens())
-            if (item.isAtrasado()) {
-                livroPesquisa.add(item);
-            }
-        }
-        return livroPesquisa;
-    }
-    
-    public ArrayList<Usuario> getAllUsuarioComAtraso(){
-        ArrayList<Usuario> usuarioPesquisa = new ArrayList<Usuario>();
-        for (Emprestimo emprestimo : emprestimos) {
-            for(Item item: emprestimo.getItens())
-            if (item.isAtrasado()) {
-                usuarioPesquisa.add(getUsuarioByCod(emprestimo.getCodUsuario()));
-                break;
-            }
-        }
-        return usuarioPesquisa;
-    }
-    
-    public Usuario getUsuarioByCod(String codUsuario) {
-        for (Usuario usuario : usuarios) {
-            if (usuario.getCodUsuario().equals(codUsuario)) {
-                return usuario;
-            }
-        }
-        return null;
-    }
-
-    public Livro getLivroByCod(String codLivro) {
-        for (int i = 0; i < livros.size(); i++) {
-            if (livros.get(i).getCodLivro().equals(codLivro)) {
-                return livros.get(i);
-            }
-        }
-        return null;
-    }
-    
-    public ArrayList<Livro> getLivroByNome(String nome){
-        ArrayList<Livro> livroPesquisa = new ArrayList<Livro>();
-        for (Livro livro : livros) {
-            if (livro.getNome().contains(nome)) {
-                livroPesquisa.add(livro);
-            }
-        }
-        return livroPesquisa;
-    }
-    
-    public ArrayList<Livro> getLivroByNomeAndAno(String nome, int ano){
-        ArrayList<Livro> livroPesquisa = new ArrayList<Livro>();
-        for (Livro livro : livros) {
-            if (livro.getNome().contains(nome) && livro.getAno() == ano) {
-                livroPesquisa.add(livro);
-            }
-        }
-        return livroPesquisa;
     }
 
     public void salvarLivros() {
